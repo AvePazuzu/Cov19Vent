c441f3f6920ac49c3fb99445ed57fbadfa8e02e4
eugen <e.kusowenko@gmail.com>
2020-06-23 15:19:39 +0200

    Major inflation func change

--- mathi.py
+++ mathi.py
@@ -7,15 +7,15 @@ Created on Thu Jun  4 08:01:48 2020
 """
 import math
 import matplotlib.pyplot as plt
-import time
+import numpy as np
 
 vAZ = 0.5
 
-kPC = 1
+kPC = 0.9
 
 tIns = 5
 
-tStp = 500
+tStp = 100
 
 # Calculated new inspiration time with respect of kPC
 dt = round(tIns + tIns*(math.pow(kPC, -1) -1), 4)
@@ -24,12 +24,34 @@ dt = round(tIns + tIns*(math.pow(kPC, -1) -1), 4)
 
 # isperation flow    
 y = []
-for i in range(tStp+1):
+for i in range(1, tStp+1):
     # print(i)
-    q = round((dt/tStp)*i, 4)
+    q = (dt/tStp)*i
     # print(q)
     vflr = 6*vAZ*((-1/kPC*(math.pow(dt, (-3)))*math.pow(q,2)) + ((1/kPC*(math.pow(dt, -2)))*q)) 
     y.append(vflr)
+
+t = 3
+def func(kpc): 
+    d = round(t + t*(math.pow(kpc, -1) -1), 2)
+    k = np.arange(0, d+(d/tStp), d/tStp)    
+    m = []
+    for i in k:
+        # print(i)
+        # q = (dt/tStp)*i
+        # print(q)
+        vflr = 6*vAZ*kpc*((-(1/kpc)*(math.pow(d, (-3)))*math.pow(i,2)) + ((1/kpc*(math.pow(d, -2)))*i)) 
+        m.append(vflr)
+    return m, k
+
+
+m1, k1 = func(1)
+m2, k2 = func(.9)
+
+plt.plot(k1, m1, "b")
+plt.plot(k2, m2, "b")
+plt.show()
+
     
 # cummulated sum
 y2 = []
@@ -37,7 +59,7 @@ j = 0
 for i in y:
     j =j+i*(dt/tStp)   
     y2.append(j)
-# min(y2)
+max(y2)
 # calculate percentage of cummulated sum steps
 y2p = []
 for i in range(len(y2)):
@@ -77,15 +99,19 @@ sum(g)
 
 # min(g)
 # sum(g)
-x1 = list(range(tStp-1))
-x = list(range(tStp+1))
+x1 = list(range(tStp))
+x = list(range(tStp))
+xx = list(range(3))
 # p1 = plt.plot(hh, x)
 p2 = plt.plot(x1, g)
 
 plt.figure(1)
 plt.plot(x, y, "r")    
 plt.plot(x, y2, "b")
+plt.plot(x, y2, "b")
 plt.show()
+
+
 
 # p1 = plt.plot(x,y)    
 
